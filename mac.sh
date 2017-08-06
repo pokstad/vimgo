e883ab57eab2cf516df61a52c8de824f5f025ef9
Paul Okstad <pokstad@gmail.com>
2017-08-05 22:58:04 -0700

    Add files via upload

---
+++ mac.sh
@@ -0,0 +1,42 @@
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Update homebrew
+brew update
+
+# Install latest VIM and override OEM VI
+brew install vim --with-override-system-vi --with-lua
+
+# Install ctags
+brew install ctags
+
+# Install minimal VIM plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
+vim +PlugInstall +qall
+
+# Install Go plugins
+git clone https://github.com/fatih/vim-go.git ~/.vim/plugged/vim-go
+
+# Install Go tools
+go get -u golang.org/x/tools/cmd/godoc 
+go get -u github.com/nsf/gocode 
+go get -u golang.org/x/tools/cmd/goimports 
+go get -u github.com/rogpeppe/godef 
+go get -u golang.org/x/tools/cmd/gorename 
+go get -u github.com/golang/lint/golint 
+go get -u github.com/kisielk/errcheck 
+go get -u github.com/jstemmer/gotags 
+go get -u github.com/tools/godep 
+
+# Copy color schemes
+mkdir -p ~/.vim/colors
+cp ./molokai.vim ~/.vim/colors/
+
+# Install real time autocomplete for vim (https://github.rom/Shougo/neocomplete.vim)
+mkdir -p ~/.vim/
+cd ~/.vim/
+git clone https://github.com/Shougo/neocomplete.vim.git
+
+# Install T-MUX
+brew install tmux
+
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Packages and tools
+
+The following tools are installed in these scripts:
+
+## VIM Go
+
+## Tagbar
+
+The tagbar is mapped to F8:
+
+`nmap <F8> :TagbarToggle<CR>`
+
+TagBar shows an outline of the current package and allows easy navigation via symbols.
+
+# Nerd Tree
+
+Nerd Tree is a file viewer we can access with F7:
+
+`nmap <F7> :NERDTreeTabsToggle<CR>`
+
+Hidden files can be revealed with `I`.
+
+Move the root dir up with `u`.
+
+Change the working dir to the selected dir with `cd`.
+
+Change the nerdtree dir to the selected dir with `C`.
+
+
+# Tabs
+
+Left and right changes tabs in normal mode:
+
+```
+nmap <F2> :tabnew<CR>
+nmap <F3> :tabclose<CR>
+nmap <Left> :tabprevious<CR>
+nmap <Right> :tabnext<CR>
+```
+
